f0b35dcb006e649073d0215829c3b355c3d034c9
Arsentiy <33813136+YellowCloudlet@users.noreply.github.com>
2018-09-22 10:58:29 +0300

    Add files via upload

---
+++ package.scala
@@ -0,0 +1,20 @@
+package object home_app { //дополнительные фичи типа возможности вызова корня каким то другим символом
+
+ /* implicit class IntAddinitional(s: Int) {
+    def /^(Int): Int = scala.math.sqrt(s)
+  }*/
+
+  /*implicit class IntAddinitional(s: Int) {
+    def \\(a: Int): Int = s + scala.math.sqrt(a)
+  }*/
+
+  /*case class A(i: Int)
+  case class B(i: Int)
+
+  implicit def aToB(a: A): B = B(a.i)
+
+  val a = A(1)
+  val b: B = a
+  println(b)*/
+
+}
