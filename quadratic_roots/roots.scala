1e9b15cbe5d3ca20bceb5f763222d045d74c2650
Arsentiy <33813136+YellowCloudlet@users.noreply.github.com>
2018-12-01 01:03:21 +0200

    Create roots.scala

---
+++ quadratic_roots/roots.scala
@@ -0,0 +1,33 @@
+  implicit val const: Double = 4 //const
+  val a = 1
+  val b = 2
+  val c = 1
+  println(s"Вычисление корней квадратного уравнения a(_)X^2 + b(_)X + c(_)")
+
+  val result: Double = discriminant(Some(a))(Some(b))(Some(c))
+  //println(s"Вычисление дискриминанта... ${scala.math.sqrt(result)}")
+
+  val tempRoots:Double = result
+
+  val rootNumber = rootNumberFunction(tempRoots)
+  println(rootNumber)
+
+  val exit:Any =  rootCalculation(Some(a))(Some(b))(Some(c))(result)
+
+  def discriminant(a: Option[Int])(b: Option[Int])(c: Option[Int])(implicit const: Double): Double = b.getOrElse(0) * b.getOrElse(0) - const * a.getOrElse(0) * c.getOrElse(0)
+
+  def rootNumberFunction(roots: Double): Any = if (roots > 0) print("2 different roots") else if (roots < 0) print("no roots") else print("2 equivalent roots")
+
+  case class B(x: Option[Int], y: Option[Int])
+
+  def rootCalculation(a: Option[Int])(b: Option[Int])(c: Option[Int])(d: Double)(implicit const: Double): Any = {
+    val firstParameter = a.getOrElse(0)
+    val secondPatameter = b.getOrElse(0)
+    val thirdParameter = c.getOrElse(0)
+    val result: Double = discriminant(Some(2))(Some(10))(Some(2))
+    println(s"Вычисление дискриминанта... ${scala.math.sqrt(result)}")
+  if (firstParameter == 0) print("Первый параметр не может быть нулевым!!! ")
+  else if (d > 0) print(s"1'st root is ${(-secondPatameter + result) / 2 * firstParameter } , 2'd root is ${(-secondPatameter - result) / 2 * firstParameter }") //return Some(x1,x2)
+  else if (d < 0) print(s"no roots found")
+  else print(s"the root is ${(-secondPatameter + result) / 2 * firstParameter}")
+}
